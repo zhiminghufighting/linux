e62bdcfbf4b05b6d9b64d5595d5738a7dd43e6a5
Andi Kleen <ak@linux.intel.com>
2021-11-05 13:19:12 -0700

    x86/tdx: Disable CQM in guest
    ANBZ: #523
    
    commit 9a07dd035a28920fdd2b658d95f1a52618afe005 intel-github.
    
    CQM has a lot of complex code that is difficult to harden. Currently
    there is no use case for CQM in a TDX guest, so just disable it.
    
    Intel-SIG: commit 9a07dd035a28 x86/tdx: Disable CQM in guest.
    Add TDX guest kernel support.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    [ Fan Du: amend commit log ]
    Signed-off-by: Fan Du <fan.du@intel.com>
    Signed-off-by: Aubrey Li <aubrey.li@linux.intel.com>
    Reviewed-by: Artie Ding <artie.ding@linux.alibaba.com>

--- arch/x86/coco/tdx/tdx.c
+++ arch/x86/coco/tdx/tdx.c
@@ -17,6 +17,7 @@
 #include <asm/insn-eval.h>
 #include <asm/pgtable.h>
 #include <linux/pci.h>
+#include <linux/nmi.h>
 
 #define CREATE_TRACE_POINTS
 #include <asm/trace/tdx.h>
@@ -958,6 +959,7 @@ void __init tdx_early_init(void)
 	setup_clear_cpu_cap(X86_FEATURE_MTRR);
 	setup_clear_cpu_cap(X86_FEATURE_APERFMPERF);
 	setup_clear_cpu_cap(X86_FEATURE_TME);
+	setup_clear_cpu_cap(X86_FEATURE_CQM_LLC);
 
 	cc_set_vendor(CC_VENDOR_INTEL);
 	cc_mask = get_cc_mask();
@@ -984,6 +986,14 @@ void __init tdx_early_init(void)
 	x86_platform.guest.enc_status_change_finish = tdx_enc_status_changed;
 
 	legacy_pic = &null_legacy_pic;
+
+	/*
+	 * Disable NMI watchdog because of the risk of false positives
+	 * and also can increase overhead in the TDX module.
+	 * This is already done for KVM, but covers other hypervisors
+	 * here.
+	 */
+	hardlockup_detector_disable();
 
 	/*
 	 * Make sure there is a panic if something goes wrong,
